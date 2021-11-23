6418f383276c48a249075e0155031d904d428dbc
Shubham Parteti <shubhamparteti12@gmail.com>
2021-11-24 00:58:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+marked.setOptions({
+    breaks: true,
+
+  });
+
+const renderer = new marked.Renderer();
+function App(){
+
+ const [text,setText] = React.useState("");
+
+
+ return (
+    <div className="text-center px-4">
+        <h1 className="p-4">Markdown Previewer</h1>
+        <textarea onChange={(e) => setText(e.target.value)}
+        name="text" 
+        id="text" 
+        rows="10" 
+        value={text} 
+        className="textarea"></textarea>
+        <h3 className="mt-3">Output</h3>
+       <Preview markdown={text}/>
+    </div>
+);
+}
+
+function Preview({ markdown} ){
+    return (
+        <div 
+        dangerouslySetInnerHtml={{
+            __html: marked(markdown, { renderer: renderer }),
+        }}
+        id="preview" 
+        ></div>
+    )
+}
+
+
+ReactDOM.render(<App/>,document.getElementById("root"));
